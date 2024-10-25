fe7aba5d06ce1f0c8ac2f317cdbc5fb6ec152f20
Gabe4563 <gabrielmolina399876@gmail.com>
2024-10-25 12:25:09 -0300

    Add files via upload

---
+++ Class1.c
@@ -0,0 +1,26 @@
+//Comentário
+/*
+ Comentário com mais linhas
+ */
+ 
+// Biblioteca / Library
+
+#include (studio.h>
+#include <stdlib.h>
+
+// tipos primitivos
+// int,float,char, bool
+
+
+// variáveis de escapo global
+int numGlobal;
+
+// Função / Método principal
+int main(){
+	// Variáveis de escopo local
+	float numQuebrado;
+	
+	// inicialização de variáveis com valores
+	char letter = 'a';
+	bool ativo = false;
+}
