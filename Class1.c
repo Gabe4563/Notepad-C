1c2e2453a05c36bff31ff9cc313f838a85eb6b36
Gabe4563 <gabrielmolina399876@gmail.com>
2024-10-28 08:53:33 -0300

    Add files via upload

--- Class1.c
+++ Class1.c
@@ -5,9 +5,9 @@
  
 // Biblioteca / Library
 
-#include (studio.h>
+#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>    
 // tipos primitivos
 // int,float,char, bool
 
@@ -23,4 +23,16 @@ int main(){
 	// inicialização de variáveis com valores
 	char letter = 'a';
 	bool ativo = false;
+	int i;
+	
+	// Estruturas de repetição
+	// Função | Métedo
+	for(i = 0; i <= 10; i++) {
+	    printf("%d\n", i);
+	}
+	
+	while(!ativo){
+	    printf("Olá, você está ativo");
+		ativo = true;
+	}
 }
